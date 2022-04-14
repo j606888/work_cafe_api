144674a6c84a87c64b5883b1362b9a4cec1a4bc8
James <j606888@gmail.com>
2022-04-14 23:10:24 +0800

    Add Service CreateMapUrl

---
+++ app/models/map_url.rb
@@ -0,0 +1,18 @@
+class MapUrl < ApplicationRecord
+  include AASM
+
+  belongs_to :user
+
+  aasm do
+    state :created, initial: true
+    state :accept, :deny
+
+    event :do_accept do
+      transitions from: :created, to: :accept
+    end
+
+    event :do_deny do
+      transitions from: :created, to: :deny
+    end
+  end
+end
