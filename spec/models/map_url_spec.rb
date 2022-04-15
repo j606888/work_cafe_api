4034b61191658541e2c18d2108aa338e5dc90275
James <j606888@gmail.com>
2022-04-15 19:26:04 +0800

    Add constraint for MapUrl#place_id uniqness

--- spec/models/map_url_spec.rb
+++ spec/models/map_url_spec.rb
@@ -1,6 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe MapUrl, type: :model do
+  it "should validate uniqueness of :place_id" do
+    map_url = FactoryBot.create :map_url, {
+      place_id: 'some_place_id'
+    }
+
+    invalid = FactoryBot.build :map_url, {
+      place_id: map_url.place_id
+    }
+    expect(invalid).not_to be_valid
+
+    invalid.place_id = map_url.place_id + 'more_text'
+    expect(invalid).to be_valid
+  end
+
   it "should validate uniqueness of [:url, :user_id]" do
     user_1 = FactoryBot.create :user
     user_2 = FactoryBot.create :user
