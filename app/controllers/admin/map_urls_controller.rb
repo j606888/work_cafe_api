ac8d188eaccab34a1ba4c5cfff4d93d2e85afcc3
James <j606888@gmail.com>
2022-04-15 19:41:15 +0800

    Add State to query_map_urls

--- app/controllers/admin/map_urls_controller.rb
+++ app/controllers/admin/map_urls_controller.rb
@@ -2,7 +2,8 @@ class Admin::MapUrlsController < Admin::ApplicationController
   def index
     map_urls = AdminService::QueryMapUrls.new(**{
       per: params[:per],
-      page: params[:page]
+      page: params[:page],
+      state: params[:state]
     }.compact).perform
 
     render 'index', locals: { map_urls: map_urls }
