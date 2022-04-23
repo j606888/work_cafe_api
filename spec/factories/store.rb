59eff05f57b6b078e62d5e2e909a0a27847534bb
James <j606888@gmail.com>
2022-04-23 11:17:27 +0800

    Add toggle_favorite service

--- spec/factories/store.rb
+++ spec/factories/store.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :store do
-    association :resourceable, factory: :map_url
+    association :sourceable, factory: :map_url
     name { "A room" }
     url { "https://google.map.com" }
   end
