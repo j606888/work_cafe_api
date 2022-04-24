2a366cc8f243e8d20f24dfd5e4fbfe7562fbcd0d
James <j606888@gmail.com>
2022-04-24 16:49:12 +0800

    Update query_all service

--- spec/factories/store.rb
+++ spec/factories/store.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :store do
     association :sourceable, factory: :map_url
-    name { "A room" }
-    url { "https://google.map.com" }
+    sequence(:name) {|n| "Store-#{n}" }
+    sequence(:url) {|n| "https://store-#{n}.com" }
   end
 end
