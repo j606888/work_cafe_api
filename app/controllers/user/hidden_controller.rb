fae209c07c663bc8900f55c46a54105af731dce0
James <j606888@gmail.com>
2022-04-24 08:20:39 +0800

    Add hidden_store API

---
+++ app/controllers/user/hidden_controller.rb
@@ -0,0 +1,19 @@
+class User::HiddenController < User::ApplicationController
+  def index
+    stores = UserService::QueryHiddenStores.new(
+      user_id: current_user.id
+    ).perform
+
+    render 'stores/index', locals: { stores: stores }
+  end
+
+  def create
+    UserService::HideStore.new(**{
+      user_id: current_user.id,
+      store_id: params.require(:store_id)
+      reason: params[:reason]
+    }.compact).perform
+
+    head :ok
+  end
+end
