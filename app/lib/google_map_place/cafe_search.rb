8674f24a64a5e2790f4ee2a19d58efd31e99670b
James <j606888@gmail.com>
2022-07-09 22:26:48 +0800

    ✨ Add GoogleMapPlace#cafe_search

---
+++ app/lib/google_map_place/cafe_search.rb
@@ -0,0 +1,43 @@
+class GoogleMapPlace
+  class CafeSearch
+    attr_reader :data
+
+    def initialize(data)
+      @data = data
+    end
+
+    def next_page_token
+      @data['next_page_token']
+    end
+
+    def places
+      @data['results'].map do |result|
+        {
+          place_id: result['place_id'],
+          name: result['name']
+        }
+      end
+    end
+
+    def next_page
+      return unless next_page_token.present?
+
+      args = {
+        pagetoken: next_page_token
+      }
+      result = GoogleMapPlace::Api.query(:nearbysearch_service, args)
+      CafeSearch.new(result)
+    end
+
+    def self.find(location, radius, pagetoken)
+      args = {
+        type: 'cafe',
+        location: location,
+        radius: radius,
+        pagetoken: pagetoken
+      }
+      result = GoogleMapPlace::Api.query(:nearbysearch_service, args)
+      CafeSearch.new(result)
+    end
+  end
+end
