3feba475cc3761efae7cb80f45a163da25c4a74c
James <j606888@gmail.com>
2022-08-07 23:32:06 +0800

    Add query map_crawler service

---
+++ app/services/map_crawler_service/query.rb
@@ -0,0 +1,24 @@
+class MapCrawlerService::Query < Service
+  DEFAULT_LIMIT = 20
+  def initialize(lat:, lng:, limit: DEFAULT_LIMIT)
+    @lat = lat
+    @lng = lng
+    @limit = limit
+  end
+
+  def perform
+    sql = <<-SQL
+      SELECT *, earth_distance(ll_to_earth(:lat, :lng), ll_to_earth(lat, lng))::INTEGER AS distance
+      FROM map_crawlers
+      ORDER BY distance
+      LIMIT :limit
+    SQL
+    arg = {
+      lat: @lat,
+      lng: @lng,
+      limit: @limit
+    }
+    query = ActiveRecord::Base.sanitize_sql_array([sql, arg])
+    MapCrawler.find_by_sql(query)
+  end
+end
--- spec/factories/map_crawlers.rb
+++ spec/factories/map_crawlers.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :map_crawler do
-    association :user    
+    association :user
+    radius { 500 }
+    total_found { 20 }
+    new_store_count { 15 }
+    repeat_store_count { 5 }
+    blacklist_store_count { 0}
   end
 end
