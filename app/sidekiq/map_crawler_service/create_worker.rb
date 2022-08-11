a74cd47694c94f56fe8a33d335d150e69364ee18
James <j606888@gmail.com>
2022-08-11 22:45:15 +0800

    Add worker MapCrawlerService::CreateWorker

---
+++ app/sidekiq/map_crawler_service/create_worker.rb
@@ -0,0 +1,14 @@
+class MapCrawlerService::CreateWorker
+  include Sidekiq::Job
+
+  sidekiq_options retry: 0
+
+  def perform(user_id, lat, lng, radius)
+    MapCrawlerService::Create.call(
+      user_id: user_id,
+      lat: lat,
+      lng: lng,
+      radius: radius
+    )
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,6 +14,7 @@
 #
 # See https://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 require 'webmock/rspec'
+require "sidekiq/testing"
 
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
