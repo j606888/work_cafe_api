8c73dd56f91f4e44b9b5ad917067539ad187da9f
James <j606888@gmail.com>
2022-08-13 16:53:54 +0800

    create service for build format opening_hours

--- app/services/helper_modules/query_store.rb
+++ app/services/helper_modules/query_store.rb
@@ -1,10 +1,8 @@
 module HelperModules::QueryStore
   def query_store_by_id!(id)
     store = Store.find_by(id: id)
-    if store.nil?
-      raise Service::PerformFailed, "Store with id `#{id}` not found"
-    end
+    return store if store.present?
 
-    store
+    raise Service::PerformFailed, "Store with id `#{id}` not found"
   end
 end
--- app/services/store_service/query_one.rb
+++ app/services/store_service/query_one.rb
@@ -1,40 +1,12 @@
 class StoreService::QueryOne < Service
+  include QueryHelpers::QueryStore
+
 
   def initialize(place_id:)
     @place_id = place_id
   end
 
   def perform
-    store = find_store!(@place_id)
-    opening_hours = store.opening_hours
-
-    {
-      store: store,
-      opening_hours: opening_hours_with_format(opening_hours)
-    }
-  end
-
-  private
-
-  def find_store!(place_id)
-    store = Store.find_by(place_id: place_id)
-    return store if store.present?
-
-    raise Service::PerformFailed, "Store with place_id `#{place_id}` not found"
-  end
-
-  def opening_hours_with_format(opening_hours)
-    # TODO, have to deal with overnight
-    weekday_map = OpeningHour.empty_weekday_map
-    
-    opening_hours.each do |open_hour|
-      period = {
-        start: open_hour.open_time.insert(2, ":"),
-        end: open_hour.close_time.insert(2, ":")
-      }
-      weekday_map[open_hour.open_day][:periods] << period
-    end
-
-    weekday_map.values
+    find_store_by_place_id(@place_id)
   end
 end
