11e339a84d5d4e6dfd1dcaaa0ed3308dec97b5e8
James <j606888@gmail.com>
2022-08-20 18:12:10 +0800

    Drop model Favorite

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_20_070327) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_20_101113) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "cube"
   enable_extension "earthdistance"
@@ -21,15 +21,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_20_070327) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "is_delete", default: false, null: false
-  end
-
-  create_table "favorites", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.bigint "store_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["store_id"], name: "index_favorites_on_store_id"
-    t.index ["user_id"], name: "index_favorites_on_user_id"
   end
 
   create_table "hiddens", force: :cascade do |t|
@@ -153,8 +144,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_20_070327) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "favorites", "stores"
-  add_foreign_key "favorites", "users"
   add_foreign_key "hiddens", "stores"
   add_foreign_key "hiddens", "users"
   add_foreign_key "map_crawlers", "users"
