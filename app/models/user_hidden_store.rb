001b224dcb62fb50b528a66627bd29329e61b29f
James <j606888@gmail.com>
2022-09-01 22:58:56 +0800

    Add UserHiddenStore model

---
+++ app/models/user_hidden_store.rb
@@ -0,0 +1,4 @@
+class UserHiddenStore < ApplicationRecord
+  belongs_to :user
+  belongs_to :store
+end
---
+++ db/migrate/20220901144954_create_user_hidden_stores.rb
@@ -0,0 +1,10 @@
+class CreateUserHiddenStores < ActiveRecord::Migration[7.0]
+  def change
+    create_table :user_hidden_stores do |t|
+      t.references :user, null: false, foreign_key: true
+      t.references :store, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
