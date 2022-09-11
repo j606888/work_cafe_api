2751fdad22dbebcbf7bc7aa2dec6b75d27a437ba
James <j606888@gmail.com>
2022-09-11 14:56:12 +0800

    Add unique for review -> user store

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -7,4 +7,14 @@ RSpec.describe Review, type: :model do
   it { is_expected.to validate_inclusion_of(:room_volume).in_array(described_class::VALID_ROOM_VOLUMES) }
   it { is_expected.to validate_inclusion_of(:time_limit).in_array(described_class::VALID_TIME_LIMITS) }
   it { is_expected.to validate_inclusion_of(:socket_supply).in_array(described_class::VALID_SOCKET_SUPPLIES) }
+
+  it "validate unique for user & store" do
+    user = create :user
+    store = create :store
+
+    review = create :review, user: user, store: store
+    invalid = build :review, user: user, store: store
+
+    expect(invalid).not_to be_valid
+  end
 end
