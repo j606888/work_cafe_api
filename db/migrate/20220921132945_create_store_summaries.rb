f034c9873c388a6987b054ca2f86264f63f76ba3
James <j606888@gmail.com>
2022-09-21 21:37:31 +0800

    Add new Table StoreSummary

---
+++ db/migrate/20220921132945_create_store_summaries.rb
@@ -0,0 +1,21 @@
+class CreateStoreSummaries < ActiveRecord::Migration[7.0]
+  def change
+    create_table :store_summaries do |t|
+      t.references :store, null: false, foreign_key: true
+      t.integer :recommend_yes, default: 0
+      t.integer :recommend_normal, default: 0
+      t.integer :recommend_no, default: 0
+      t.integer :room_volume_quite, default: 0
+      t.integer :room_volume_normal, default: 0
+      t.integer :room_volume_loud, default: 0
+      t.integer :time_limit_no, default: 0
+      t.integer :time_limit_weekend, default: 0
+      t.integer :time_limit_yes, default: 0
+      t.integer :socket_supply_no, default: 0
+      t.integer :socket_supply_rare, default: 0
+      t.integer :socket_supply_yes, default: 0
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_21_120407) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_21_132945) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "cube"
   enable_extension "earthdistance"
@@ -134,6 +134,25 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_21_120407) do
     t.index ["store_id"], name: "index_store_sources_on_store_id"
   end
 
+  create_table "store_summaries", force: :cascade do |t|
+    t.bigint "store_id", null: false
+    t.integer "recommend_yes", default: 0
+    t.integer "recommend_normal", default: 0
+    t.integer "recommend_no", default: 0
+    t.integer "room_volume_quite", default: 0
+    t.integer "room_volume_normal", default: 0
+    t.integer "room_volume_loud", default: 0
+    t.integer "time_limit_no", default: 0
+    t.integer "time_limit_weekend", default: 0
+    t.integer "time_limit_yes", default: 0
+    t.integer "socket_supply_no", default: 0
+    t.integer "socket_supply_rare", default: 0
+    t.integer "socket_supply_yes", default: 0
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["store_id"], name: "index_store_summaries_on_store_id"
+  end
+
   create_table "stores", force: :cascade do |t|
     t.string "place_id", null: false
     t.string "name", null: false
@@ -210,6 +229,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_21_120407) do
   add_foreign_key "store_photos", "stores"
   add_foreign_key "store_photos", "users"
   add_foreign_key "store_sources", "stores"
+  add_foreign_key "store_summaries", "stores"
   add_foreign_key "third_party_logins", "users"
   add_foreign_key "user_hidden_stores", "stores"
   add_foreign_key "user_hidden_stores", "users"
