7c206cec0b7fcc1b3767e4fd4cad057a53a85807
James <j606888@gmail.com>
2022-10-08 20:02:00 +0800

    Add tags to review_report

--- app/services/store_service/review_report.rb
+++ app/services/store_service/review_report.rb
@@ -21,6 +21,15 @@ class StoreService::ReviewReport < Service
       report[:socket_supply][review.socket_supply.to_sym] += 1 if review.socket_supply
     end
 
+    tags_array = build_tags_array(store)
+
+    report[:primary_tags] = tags_array.filter do |tag_info|
+      tag_info[:primary]
+    end
+    report[:secondary_tags] = tags_array.filter do |tag_info|
+      !tag_info[:primary]
+    end
+
     report
   end
 
@@ -28,5 +37,23 @@ class StoreService::ReviewReport < Service
 
   def init_count(options)
     options.map(&:to_sym).index_with(0)
+  end
+
+  def build_tags_array(store)
+    tags = store.tags
+    tag_map = tags.each_with_object({}) do |tag, memo|
+      memo[tag.id] = {
+        primary: tag.primary,
+        name: tag.name,
+        count: 0
+      }
+      memo
+    end
+
+    store.store_review_tags.each do |store_review_tag|
+      tag_map[store_review_tag.tag_id][:count] += 1
+    end
+
+    tag_map.values
   end
 end
--- spec/services/store_service/review_report_spec.rb
+++ spec/services/store_service/review_report_spec.rb
@@ -3,11 +3,12 @@ require 'rails_helper'
 describe StoreService::ReviewReport do
   let!(:users) { create_list :user, 5 }
   let!(:store) { create :store}
+  let!(:tags) { create_list :tag, 5 }
   let(:params) { { store_id: store.id } }
   let(:service) { described_class.new(**params) }
 
-  def create_review(user, recommend, room_volume=nil, time_limit=nil, socket_supply=nil)
-    create :review, {
+  def create_review(user, recommend, room_volume=nil, time_limit=nil, socket_supply=nil, tags=[])
+    review = create :review, {
       user: user,
       store: store,
       recommend: recommend,
@@ -15,12 +16,17 @@ describe StoreService::ReviewReport do
       time_limit: time_limit,
       socket_supply: socket_supply
     }
+    tags.each do |tag|
+      create :store_review_tag, store: store, review: review, tag: tag
+    end
+
+    review
   end
 
   before do
-    create_review(users[0], 'yes', nil, 'yes', 'rare')
+    create_review(users[0], 'yes', nil, 'yes', 'rare', [tags[0], tags[1]])
     create_review(users[1], 'no', 'loud', 'weekend', nil)
-    create_review(users[2], 'yes', 'loud', 'weekend', nil)
+    create_review(users[2], 'yes', 'loud', 'weekend', nil, [tags[0], tags[4]])
     create_review(users[3], 'normal', 'loud', 'yes', 'yes')
     create_review(users[4], 'yes', nil, 'weekend', 'yes')
   end
@@ -52,7 +58,25 @@ describe StoreService::ReviewReport do
         no: 0,
         rare: 1,
         yes: 2
-      }
+      },
+      primary_tags: [],
+      secondary_tags: [
+        {
+          primary: false,
+          name: tags[0].name,
+          count: 2
+        },
+        {
+          primary: false,
+          name: tags[1].name,
+          count: 1
+        },
+        {
+          primary: false,
+          name: tags[4].name,
+          count: 1
+        }
+      ]
     }
     expect(res).to eq(expect_res)
   end
