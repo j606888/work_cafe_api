dd84c2e80a36b501d76bba4d72c218cde0eba131
James <j606888@gmail.com>
2022-10-15 15:33:26 +0800

    Fix error

--- db/migrate/20221015070956_drop_store_photo_groups.rb
+++ db/migrate/20221015070956_drop_store_photo_groups.rb
@@ -1,9 +1,5 @@
 class DropStorePhotoGroups < ActiveRecord::Migration[7.0]
   def change
-    StorePhotoGroup.all.each do |g|
-      g.store_photos.delete_all
-    end
-
     remove_column :store_photos, :store_photo_group_id
     drop_table :store_photo_groups
   end
