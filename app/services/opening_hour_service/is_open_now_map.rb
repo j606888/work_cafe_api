afed99da1184216129f23d4cd1d79a6c0a1f3ae8
James <j606888@gmail.com>
2023-02-12 19:22:26 +0800

    Tweak timing

--- app/services/opening_hour_service/is_open_now_map.rb
+++ app/services/opening_hour_service/is_open_now_map.rb
@@ -11,8 +11,10 @@ class OpeningHourService::IsOpenNowMap < Service
     open_store_ids = OpeningHour.where(store_id: @store_ids, open_day: open_day, close_day: open_day)
       .where("open_time <= ? AND close_time >= ?", cur_time, cur_time)
       .pluck(:store_id)
+    open_to_mid_night_ids = OpeningHour.where(store_id: @store_ids, open_day: open_day, close_day: (open_day + 1) % 7)
+      .where("open_time <= :cur_time", cur_time: cur_time).pluck(:store_id)
     open_24_store_ids = OpeningHour.where(store_id: @store_ids, open_day: 0, open_time: '0000', close_day: nil, close_time: nil).pluck(:store_id)
-    is_open_map = (open_store_ids + open_24_store_ids).uniq.index_with(true)
+    is_open_map = (open_store_ids + open_24_store_ids + open_to_mid_night_ids).uniq.index_with(true)
 
     @store_ids.each_with_object({}) do |store_id, memo|
       memo[store_id] = is_open_map[store_id].present?
